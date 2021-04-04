5bd08cc0f730c69805383aff0e6284a2c6998f93
musabalki <musabalkimb@gmail.com>
2021-04-04 23:44:40 +0300

    Context öncesi componentler oluşturuldu

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import BookList from "./BookList"
+
+class App extends React.Component {
+    render(){
+        return (
+            <div><BookList books={this.state.books} /></div>
+        )
+    }
+}
+
+export default App;
